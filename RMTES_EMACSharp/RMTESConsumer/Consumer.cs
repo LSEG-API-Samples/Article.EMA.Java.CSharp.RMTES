70087dff9107972ec90bad845985d1a202e8ee00
Wasin Waeosri <wasin.waeosri@refinitiv.com>
2024-10-04 17:24:36 +0700

    Add Consumer Code

--- RMTES_EMACSharp/RMTESConsumer/Consumer.cs
+++ RMTES_EMACSharp/RMTESConsumer/Consumer.cs
@@ -8,11 +8,128 @@
 
 namespace RMTESConsumer;
 
+using LSEG.Ema.Access;
+using System.Threading;
+using System;
+using static LSEG.Ema.Access.DataType;
+using LSEG.Ema.Rdm;
+
+internal class AppClient : IOmmConsumerClient
+{
+    public void OnRefreshMsg(RefreshMsg refreshMsg, IOmmConsumerEvent consumerEvent)
+    {
+        Console.WriteLine("Item Name: " + (refreshMsg.HasName ? refreshMsg.Name() : "<not set>"));
+        Console.WriteLine("Service Name: " + (refreshMsg.HasServiceName ? refreshMsg.ServiceName() : "<not set>"));
+
+        Console.WriteLine("Item State: " + refreshMsg.State());
+
+        if (DataType.DataTypes.FIELD_LIST == refreshMsg.Payload().DataType)
+            Decode(refreshMsg.Payload().FieldList());
+
+        Console.WriteLine();
+    }
+
+    public void OnUpdateMsg(UpdateMsg updateMsg, IOmmConsumerEvent consumerEvent)
+    {
+        Console.WriteLine("Item Name: " + (updateMsg.HasName ? updateMsg.Name() : "<not set>"));
+        Console.WriteLine("Service Name: " + (updateMsg.HasServiceName ? updateMsg.ServiceName() : "<not set>"));
+
+        if (DataTypes.FIELD_LIST == updateMsg.Payload().DataType)
+            Decode(updateMsg.Payload().FieldList());
+
+        Console.WriteLine();
+    }
+
+    public void OnStatusMsg(StatusMsg statusMsg, IOmmConsumerEvent consumerEvent)
+    {
+
+        Console.WriteLine("Item Name: " + (statusMsg.HasName ? statusMsg.Name() : "<not set>"));
+        Console.WriteLine("Service Name: " + (statusMsg.HasServiceName ? statusMsg.ServiceName() : "<not set>"));
+
+        if (statusMsg.HasState)
+            Console.WriteLine("Item State: " + statusMsg.State());
+
+        Console.WriteLine();
+    }
+
+    void Decode(FieldList fieldList)
+    {
+        foreach (FieldEntry fieldEntry in fieldList)
+        {
+            Console.Write("Fid: " + fieldEntry.FieldId + " Name = " + fieldEntry.Name + " DataType: " + DataType.AsString(fieldEntry.Load!.DataType) + " Value: ");
+
+            if (Data.DataCode.BLANK == fieldEntry.Code)
+                Console.WriteLine(" blank");
+            else
+                switch (fieldEntry.LoadType)
+                {
+                    case DataTypes.REAL:
+                        Console.WriteLine(fieldEntry.OmmRealValue().AsDouble());
+                        break;
+                    case DataTypes.ASCII:
+                        Console.WriteLine(fieldEntry.OmmAsciiValue());
+                        break;
+                    case DataTypes.ENUM:
+                        Console.WriteLine(fieldEntry.HasEnumDisplay ? fieldEntry.EnumDisplay() : fieldEntry.EnumValue());
+                        break;
+                    case DataTypes.RMTES:
+                        Console.WriteLine(fieldEntry.OmmRmtesValue());
+                        break;
+                    case DataTypes.ERROR:
+                        Console.WriteLine("(" + fieldEntry.OmmErrorValue().ErrorCodeAsString() + ")");
+                        break;
+                    default:
+                        Console.WriteLine();
+                        break;
+                }
+        }
+    }
+}
 class Consumer
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadLine();
+        // Crate OmmConsumer class
+        OmmConsumer? consumer = null;
+        try
+        {
+            // Initiate Consumer AppClient class
+            AppClient appClient = new();
+
+            Console.WriteLine("Consumer: Start");
+            // Init connection to the Provider
+            OmmConsumerConfig config = new OmmConsumerConfig().Host("192.168.56.1:14002").UserName("user");
+            consumer = new OmmConsumer(config);
+
+            // Construct the View Request
+            OmmArray array = new()
+            {
+                FixedWidth = 2
+            };
+
+            array.AddInt(15)
+               .AddInt(22)
+               .AddInt(25)
+               .AddInt(260)
+               .AddInt(1352)
+               .Complete();
+
+            var view = new ElementList()
+                .AddUInt(EmaRdm.ENAME_VIEW_TYPE, 1)
+                .AddArray(EmaRdm.ENAME_VIEW_DATA, array)
+                .Complete();
+
+            Console.WriteLine("Consumer: Sending Item request");
+            consumer.RegisterClient(new RequestMsg().ServiceName("DIRECT_FEED").Name("/LSEG.L").Payload(view), appClient);
+            Thread.Sleep(60000); // API calls OnRefreshMsg(), OnUpdateMsg() and OnStatusMsg()
+        }
+        catch (OmmException excp)
+        {
+            Console.WriteLine(excp.Message);
+        }
+        finally
+        {
+            consumer?.Uninitialize();
+        }
     }
 }
